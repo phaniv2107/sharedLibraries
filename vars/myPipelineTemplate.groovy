4aba36f7bcbbbf3bd4caf982b64df56c37be918a
phaniv2107 <phaniv2107@gmail.com>
2025-10-15 07:15:57 +0000

    commit

---
+++ vars/myPipelineTemplate.groovy
@@ -0,0 +1,35 @@
+    def call(Map config) {
+        pipeline {
+            agent any
+            stages {
+                stage('Checkout') {
+                    steps {
+                        git url: config.gitRepo, branch: config.gitBranch
+                    }
+                }
+                stage('Build') {
+                    steps {
+                        script {
+                            // Example build command based on config
+                            if (config.buildTool == 'maven') {
+                                sh "mvn clean install"
+                            } else if (config.buildTool == 'gradle') {
+                                sh "gradle build"
+                            }
+                        }
+                    }
+                }
+                stage('Test') {
+                    steps {
+                        sh "run-tests.sh" // Assuming a script in your project
+                    }
+                }
+                // Add more stages as needed
+            }
+            post {
+                always {
+                    echo "Pipeline finished for project: ${config.projectName}"
+                }
+            }
+        }
+    }
