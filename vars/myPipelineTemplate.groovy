071e7c04f93b84d29dd8d82a6829793d6e820e60
phaniv2107 <phaniv2107@gmail.com>
2025-10-29 16:29:10 +0530

    Update myPipelineTemplate.groovy

--- vars/myPipelineTemplate.groovy
+++ vars/myPipelineTemplate.groovy
@@ -10,19 +10,28 @@
                 stage('Build') {
                     steps {
                         script {
-                              // sh 'cd demo-java-app
-                               sh '''ls -lrt                          
-                                     cd java-maven-sonar-argocd-helm-k8s/spring-boot-app'
-                                     ls -lrt'''
-                            // Example build command based on config
-                            if (config.buildTool == 'maven') {
-                                sh "mvn clean install"
-                            } else if (config.buildTool == 'gradle') {
-                                sh "gradle build"
-                            }
+                            // Navigate and list contents
+                            sh '''
+                            ls -lrt
+                            cd java-maven-sonar-argocd-helm-k8s/spring-boot-app
+                            ls -lrt
+                            '''
+
+                        // Example build command based on config
+                        if (config.buildTool == 'maven') {
+                            sh '''
+                            cd java-maven-sonar-argocd-helm-k8s/spring-boot-app
+                            mvn clean install
+                            '''
+                         } else if (config.buildTool == 'gradle') {
+                            sh '''
+                            cd java-maven-sonar-argocd-helm-k8s/spring-boot-app
+                            gradle build
+                            '''
                         }
                     }
                 }
+            }
                 stage('Test') {
                     steps {
                         sh "run-tests.sh" // Assuming a script in your project
